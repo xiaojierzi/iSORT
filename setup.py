551b8a7ac9dad4d8c65f00db52c33781bb53be68
Yecheng Tan <74916789+xiaojierzi@users.noreply.github.com>
2024-01-23 07:17:00 +0800

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+
+setup(
+    name="iSORT",
+    version="1.0.0",
+    description='iSORT: an Integrative Method for Reconstructing Spatial Organization of Cells via Transfer Learning',
+    author='Yecheng Tan',
+    author_email='yctan21@m.fudan.edu.cn',
+    url='',
+    packages=['iSORT'],
+    install_requires=[],
+    python_requires='>=3.8',
+    license='GNU General Public License v3.0',
+)
+
+

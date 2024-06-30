b2a0e22c5763e8ed3483f2078238cba38b719d0a
Yecheng Tan <74916789+xiaojierzi@users.noreply.github.com>
2024-06-30 22:07:57 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,15 @@
 from setuptools import setup
 
-
 setup(
-    name="iSORT",
-    version="1.0.0",
-    description='iSORT: an Integrative Method for Reconstructing Spatial Organization of Cells via Transfer Learning',
+    name="iSORTpy",
+    version="1.0.1",
+    description='Transfer learning of multicellular organization via single-cell and spatial transcriptomics',
     author='Yecheng Tan',
     author_email='yctan21@m.fudan.edu.cn',
-    url='',
+    url='https://github.com/xiaojierzi/iSORT',
     packages=['iSORT'],
     install_requires=[],
     python_requires='>=3.8',
     license='GNU General Public License v3.0',
 )
-
 
